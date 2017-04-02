a94d98191bcf070814ef4d19ad016e1034e5b04c
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2017-04-02 15:29:50 +0530

    Database details updated.

--- app/config/db.php
+++ app/config/db.php
@@ -2,8 +2,8 @@
 
 global $db_name, $db_user, $db_pass, $db_host;
 
-$db_name = "bhasifsz_p1kachu";
-$db_user = "bhasifsz_t0g3p1";
+$db_name = "musourav_p1kachu";
+$db_user = "musourav_t0g3p1";
 $db_pass = "b3nd3raz";
 $db_host = "localhost";
 
